85388a5c914e7a934cc1ad7d2958c32d246374b2
Hisun0 <tridvad1@gmail.com>
2022-12-09 11:23:24 +0300

    added name generator

---
+++ nameGenerator/bin/nameGenerator.js
@@ -0,0 +1,4 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Name Generator!\nDo you want to generate uniq name?');
+const answer = readlineSync.question('Type "yes" to continue: ');
