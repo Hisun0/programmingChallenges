b35d98bdfd9430df03159d3997971c0e54ec37be
Hisun0 <tridvad1@gmail.com>
2022-12-12 13:07:59 +0300

    new architecture of repository

--- nameGenerator/bin/nameGenerator.js
+++ nameGenerator/bin/nameGenerator.js
@@ -10,14 +10,14 @@ function nameGenerator() {
     console.log('Welcome to the Name Generator!\nDo you want to generate uniq name?');
     const answer = readlineSync.question('Type "yes" to continue: ');
     if (answer.toLowerCase().trim() !== 'yes') {
-        return 'Ну и иди в пизду';
+        return 'If you want to generate random name, you need to answer "yes"';
     } 
     const answers = setQuestions();
     
     if (answers[1].toLowerCase().trim() === 'ru') return capitalize(randomLetter(vowelsRU, consonantsRU, answers[0]));
     if (answers[1].toLowerCase().trim() === 'en') return capitalize(randomLetter(vowelsENG, consonantsENG, answers[0]));
 
-    return 'надо тренироваться бро';
+    return 'Error: undefined language!';
 }
 
 console.log(nameGenerator());
